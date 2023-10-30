ff4506b9a922233c27ab16ae4a146d6751afb891
kervy-cantos <krteammain@gmail.com>
2023-10-30 22:45:29 +0800

    create profile page

---
+++ app/(root)/profile/[id]/page.tsx
@@ -0,0 +1,67 @@
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { profileEnd } from "console";
+import { profileTabs } from "@/app/constants";
+import Image from "next/image";
+import PostsTab from "@/components/shared/PostsTab";
+
+
+
+
+
+const Page = async ({ params }: { params: { id: string } }) => {
+	const user = await currentUser();
+	if (!user) return null
+
+	const userInfo = await fetchUser(params.id);
+	if (!userInfo.onboarded) return redirect('/onboard')
+	return (
+		<section>
+			<ProfileHeader
+				accountId={userInfo.id}
+				authUserId={user.id}
+				name={userInfo.name}
+				username={userInfo.username}
+				imgUrl={userInfo.image}
+				bio={userInfo.bio}
+			/>
+			<div className="mt-9">
+				<Tabs defaultValue="threads" className="w-full">
+					<TabsList className="tab">
+						{profileTabs.map((tab) => (
+							<TabsTrigger>
+								<Image
+									src={tab.icon}
+									alt={tab.label}
+									height={24}
+									width={24}
+									className="object-contain"
+								/>
+								<p className="max-sm:hidden">{tab.label}</p>
+								{tab.label == 'Threads' && (
+									<p className="ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2">
+										{userInfo?.posts?.length}
+									</p>
+								)}
+							</TabsTrigger>
+						))}
+
+					</TabsList>
+					{profileTabs.map((tab) => (
+						<TabsContent key={`content-${tab.label}`} value={tab.value} className="w-full text-light-1">
+							<PostsTab
+								currentUserId={user.id}
+								accountId={userInfo.id}
+								accountType="User"
+							/>
+						</TabsContent>
+					))}
+				</Tabs>
+			</div>
+		</section>);
+};
+
+export default Page;
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,58 +1,75 @@
-"use server";
+'use server';
 
-import { FilterQuery, SortOrder } from "mongoose";
-import { revalidatePath } from "next/cache";
+import { FilterQuery, SortOrder } from 'mongoose';
+import { revalidatePath } from 'next/cache';
 
-import User from "../models/user.model";
+import User from '../models/user.model';
 
-import { connectToDatabase } from "../mongoose";
+import { connectToDatabase } from '../mongoose';
+import Post from '../models/post.model';
 
 export async function fetchUser(userId: string) {
-  try {
-    connectToDatabase();
+	try {
+		connectToDatabase();
 
-    return await User.findOne({ id: userId });
-  } catch (error: any) {
-    throw new Error(`Failed to fetch user: ${error.message}`);
-  }
+		return await User.findOne({ id: userId });
+	} catch (error: any) {
+		throw new Error(`Failed to fetch user: ${error.message}`);
+	}
 }
 
 interface Params {
-  userId: string;
-  username: string;
-  name: string;
-  bio: string;
-  image: string;
-  path: string;
+	userId: string;
+	username: string;
+	name: string;
+	bio: string;
+	image: string;
+	path: string;
 }
 
-export async function updateUser({
-  userId,
-  bio,
-  name,
-  path,
-  username,
-  image,
-}: Params): Promise<void> {
-  try {
-    connectToDatabase();
+export async function updateUser({ userId, bio, name, path, username, image }: Params): Promise<void> {
+	try {
+		connectToDatabase();
 
-    await User.findOneAndUpdate(
-      { id: userId },
-      {
-        username: username.toLowerCase(),
-        name,
-        bio,
-        image,
-        onboarded: true,
-      },
-      { upsert: true }
-    );
+		await User.findOneAndUpdate(
+			{ id: userId },
+			{
+				username: username.toLowerCase(),
+				name,
+				bio,
+				image,
+				onboarded: true
+			},
+			{ upsert: true }
+		);
 
-    if (path === "/profile/edit") {
-      revalidatePath(path);
-    }
-  } catch (error: any) {
-    throw new Error(`Failed to create/update user: ${error.message}`);
-  }
+		if (path === '/profile/edit') {
+			revalidatePath(path);
+		}
+	} catch (error: any) {
+		throw new Error(`Failed to create/update user: ${error.message}`);
+	}
+}
+
+export async function fetchUserPosts(userId: string) {
+	try {
+		connectToDatabase();
+
+		const posts = await User.findOne({ id: userId }).populate({
+			path: 'posts',
+			model: Post,
+			populate: {
+				path: 'children',
+				model: Post,
+				populate: {
+					path: 'author',
+					model: User,
+					select: 'name image id'
+				}
+			}
+		});
+		return posts;
+	} catch (error: any) {
+		throw new Error(`Failed to fetch posts: ${error.message}`);
+	}
 }
